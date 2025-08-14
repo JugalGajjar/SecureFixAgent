3fd27f265d73e33ba80974e0bdd6a4a6e9b28393
Jugal Gajjar <812jugalgajjar@gmail.com>
2025-08-14 14:38:00 -0400

    Generated the patched version of test_vuln.py

--- data/fixed/test_vuln.py
+++ data/fixed/test_vuln.py
@@ -4,3 +4,5 @@ def unsafe():
     user_input = input("Enter command: ")
     # Use subprocess.run with shell=False to prevent command injection
     subprocess.run(user_input, check=True, shell=False)
+
+unsafe()
